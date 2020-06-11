48f65b766144f69e54bcbe82d35a5f4680ab081f
Paul <paelliot@gmail.com>
2020-06-11 17:42:16 +0100

    initial commit

---
+++ 297-Simulate5SidedDie/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Dataflow;
+
+namespace _297_Simulate5SidedDie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Approach 1");
+            // as we need to generate lots of numbers in quick succession we want to use the same Random() as
+            // any randoms created to close together will produce the same series of numbers
+
+            Random rnd = new Random();
+            List<int> results = new List<int>();
+            results.Add(0);
+            results.Add(0);
+            results.Add(0);
+            results.Add(0);
+            results.Add(0);
+            results.Add(0);
+            results.Add(0);
+            results.Add(0);
+            results.Add(0);
+            results.Add(0);
+
+            Console.WriteLine("Generating distribution of numbers from rand7() 100,000,000 times");
+            for (int i = 0; i < 100000001; i++)
+            {
+                int result = RandomNumberGenerator.rand7_approach1(rnd);
+                results[result]++;
+
+                if (i % 10000000 == 0 && i != 0)
+                    Console.WriteLine("Generated " + i + " results");
+            }
+
+            WriteResults(results);
+
+            Console.WriteLine("Generating distribution of numbers from rand5() 100,000,000 times");
+            for (int i = 0; i < 100000001; i++)
+            {
+                int result = RandomNumberGenerator.rand5_approach1(rnd);
+                results[result]++;
+
+                if (i % 10000000 == 0 && i != 0)
+                    Console.WriteLine("Generated " + i + " results");
+            }
+
+            WriteResults(results);
+
+
+            Console.WriteLine("************************");
+            Console.WriteLine("Approach 3");
+
+            Console.WriteLine("Generating distribution of numbers from rand7() 100,000,000 times");
+            for (int i = 0; i < 100000001; i++)
+            {
+                int result = RandomNumberGenerator.rand7_approach2(rnd);
+                results[result]++;
+
+                if (i % 10000000 == 0 && i != 0)
+                    Console.WriteLine("Generated " + i + " results");
+            }
+
+            WriteResults(results);
+
+
+            Console.WriteLine("Generating distribution of numbers from rand5() 100,000,000 times");
+            for (int i = 0; i < 100000001; i++)
+            {
+                int result = RandomNumberGenerator.rand5_approach2(rnd);
+                results[result]++;
+
+                if (i % 10000000 == 0 && i != 0)
+                    Console.WriteLine("Generated " + i + " results");
+            }
+
+            WriteResults(results);
+        }
+
+        private static void WriteResults(List<int> results)
+        {
+            for (int i = 0; i < results.Count; i++)
+            {
+                Console.WriteLine(i + ": " + results[i]);
+                results[i] = 0;
+            }
+        }
+    }
+}
---
+++ 297-Simulate5SidedDie/RandomNumberGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace _297_Simulate5SidedDie
+{
+    static class RandomNumberGenerator
+    {
+        public static int rand7_approach1(Random rnd)
+        {
+            return rnd.Next(1, 8);   // creates a number between 1 and 7
+        }
+
+        public static int rand5_approach1(Random rnd)
+        {
+            int i = 7;
+            while (i > 5)
+                i = rand7_approach1(rnd);
+
+            return i;
+        }
+
+        public static int rand7_approach2(Random rnd)
+        {
+            int i = rand7_approach2(rnd, false);
+            double d = i / 10;
+            int result = int.Parse(Math.Ceiling(d).ToString());
+
+            return result;
+        }
+        public static int rand7_approach2(Random rnd, bool for5)
+        {
+            // creates a number between 0 and 69. 
+            // As we are dividing result by 10 we want 0.0 -> 0.9 to be the range for each integer
+            // doing 1 - 70 / 10 will not give the same mathematical distribution for each integer
+            return rnd.Next(0, 70);   
+        }
+
+        public static int rand5_approach2(Random rnd)
+        {
+            int i = rand7_approach2(rnd, true);
+            double d = i / 10;
+            int result = 0;
+
+            if (d > 0.0 & d <= 1.4) result = 1;
+            else if (d > 1.4 & d <= 2.8) result = 2;
+            else if (d > 2.8 & d <= 4.2) result = 3;
+            else if (d > 4.2 & d <= 5.6) result = 4;
+            else if (d > 5.6 & d <= 7.0) result = 5;
+
+            return result;
+        }
+    }
+}
